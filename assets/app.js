24f522df1e07bc43ee97558c60efe5d8265502f9
白河愁 <kryso@21cn.com>
2026-07-05 03:50:18 +0900

    Port original player motion 26 handler

--- assets/app.js
+++ assets/app.js
@@ -207,7 +207,7 @@ function inputBits() {
 }
 
 async function loadWasm() {
-  const response = await fetch("../game_core.197172.wasm");
+  const response = await fetch("../game_core.197438.wasm");
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
   return result.instance.exports;
