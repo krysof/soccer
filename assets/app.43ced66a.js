8483bb96bc4aa19f391518fc71da36b16c0b1745
白河愁 <kryso@21cn.com>
2026-07-20 20:38:08 +0900

    Publish original AI RAM alias fix

--- assets/app.43ced66a.js
+++ assets/app.43ced66a.js
@@ -767,7 +767,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.3ec1341f.wasm";
+  const relative = "../soccer_core_cpp.0ab98469.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
