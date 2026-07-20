f61e3a30d9c3aaaf8bce2f8a99cda2e1a660c664
白河愁 <kryso@21cn.com>
2026-07-20 20:32:59 +0900

    Publish source-coordinate presentation sync

--- assets/app.43ced66a.js
+++ assets/app.43ced66a.js
@@ -767,7 +767,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.df682d11.wasm";
+  const relative = "../soccer_core_cpp.3ec1341f.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
