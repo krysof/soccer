fa08855e8f93c524f188979318144b99530031f5
白河愁 <kryso@21cn.com>
2026-07-20 22:14:28 +0900

    Publish C++ only runtime

--- assets/app.d3eef445.js
+++ assets/app.d3eef445.js
@@ -767,7 +767,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.f9d62bd8.wasm";
+  const relative = "../soccer_core_cpp.72277cb0.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
