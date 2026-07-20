99a8b48f87f35a826a3a82c541bbc9ec715ed6cf
白河愁 <kryso@21cn.com>
2026-07-20 20:54:19 +0900

    Publish original score and victory frames

--- assets/app.bec78c3a.js
+++ assets/app.bec78c3a.js
@@ -767,7 +767,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.bd41dfdc.wasm";
+  const relative = "../soccer_core_cpp.41325740.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
