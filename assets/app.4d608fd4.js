e22ebc3f941cb0eed079a7ad4d040688e18d9c22
白河愁 <kryso@21cn.com>
2026-07-21 04:00:07 +0900

    Publish A147 847F camera continuations

--- assets/app.4d608fd4.js
+++ assets/app.4d608fd4.js
@@ -767,7 +767,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.bc6d0531.wasm";
+  const relative = "../soccer_core_cpp.a31cfdc8.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
