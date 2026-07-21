5a33b5fe278a8d1b17685e990713a22baf921183
白河愁 <kryso@21cn.com>
2026-07-21 14:39:52 +0900

    Publish isolated sound execution whitelist

--- assets/app.bec78c3a.js
+++ assets/app.bec78c3a.js
@@ -674,7 +674,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.5d45c2b0.wasm";
+  const relative = "../soccer_core_cpp.42b0fd8d.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
