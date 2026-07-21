78988e367364c700bf753c8a91e8f10a0191225b
白河愁 <kryso@21cn.com>
2026-07-21 12:04:53 +0900

    Publish complete original data migration

--- assets/app.acc42883.js
+++ assets/app.acc42883.js
@@ -674,7 +674,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.23c824e6.wasm";
+  const relative = "../soccer_core_cpp.5d45c2b0.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
