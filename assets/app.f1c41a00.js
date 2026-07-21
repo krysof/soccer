7d9dbb1173314e54eb5b40054d6d47829fb70814
白河愁 <kryso@21cn.com>
2026-07-21 16:09:30 +0900

    Publish semantic motion dispatch build

--- assets/app.f1c41a00.js
+++ assets/app.f1c41a00.js
@@ -674,7 +674,7 @@ async function loadCppCoreData(api) {
 }
 async function loadWasm() {
   const filename = "soccer_core_cpp.wasm";
-  const relative = "../soccer_core_cpp.e4081fa2.wasm";
+  const relative = "../soccer_core_cpp.5b87b991.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
