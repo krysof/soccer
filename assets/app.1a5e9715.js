92433c58e4c714f7e58d852ef5d3297029e7ecd9
白河愁 <kryso@21cn.com>
2026-07-26 17:52:34 +0900

    Publish 20260726.28 semantic ball continuation

--- assets/app.1a5e9715.js
+++ assets/app.1a5e9715.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.d924a545.wasm" : "../soccer_core_cpp.c2dbc94e.wasm";
+  const relative = DEBUG ? "../strict-tests.f4707779.wasm" : "../soccer_core_cpp.05622b61.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
