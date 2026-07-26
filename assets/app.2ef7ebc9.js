ce8211c28a34b5cd7d0a256fd347376d9ce21c23
白河愁 <kryso@21cn.com>
2026-07-26 13:21:02 +0900

    Publish 20260726.23 title camera fix

--- assets/app.2ef7ebc9.js
+++ assets/app.2ef7ebc9.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.6b252dc4.wasm" : "../soccer_core_cpp.410fdb81.wasm";
+  const relative = DEBUG ? "../strict-tests.b7a2b292.wasm" : "../soccer_core_cpp.318bc762.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
