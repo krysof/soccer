a60ddc9b7d0a4a44f486988f61de8e37988144fb
白河愁 <kryso@21cn.com>
2026-07-26 18:08:08 +0900

    Publish 20260726.29 semantic surface continuation

--- assets/app.2ef7ebc9.js
+++ assets/app.2ef7ebc9.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.f4707779.wasm" : "../soccer_core_cpp.05622b61.wasm";
+  const relative = DEBUG ? "../strict-tests.b019b998.wasm" : "../soccer_core_cpp.6bff2400.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
