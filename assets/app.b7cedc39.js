388b00b9719f01ff8ddb7ffd6179f1846e0751f9
白河愁 <kryso@21cn.com>
2026-07-28 13:38:07 +0900

    Publish 20260728.4 semantic player continuation

--- assets/app.b7cedc39.js
+++ assets/app.b7cedc39.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.876e0fd2.wasm" : "../soccer_core_cpp.799995e6.wasm";
+  const relative = DEBUG ? "../strict-tests.3afaece5.wasm" : "../soccer_core_cpp.b632f022.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
