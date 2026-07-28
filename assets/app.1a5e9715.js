437d18eb8ec73c8a240e94607aec8b92ac003e2b
白河愁 <kryso@21cn.com>
2026-07-28 12:42:49 +0900

    Publish 20260728.1 semantic field boundary

--- assets/app.1a5e9715.js
+++ assets/app.1a5e9715.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.b019b998.wasm" : "../soccer_core_cpp.6bff2400.wasm";
+  const relative = DEBUG ? "../strict-tests.454d8c5e.wasm" : "../soccer_core_cpp.e8506228.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
