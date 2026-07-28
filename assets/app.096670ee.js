5960dfc00fefc0a4ed992377fd4775385404ccd0
白河愁 <kryso@21cn.com>
2026-07-28 15:25:04 +0900

    Publish 20260728.8 semantic ball probes

--- assets/app.096670ee.js
+++ assets/app.096670ee.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.52dcc1ef.wasm" : "../soccer_core_cpp.38e8ac8e.wasm";
+  const relative = DEBUG ? "../strict-tests.f9082dcc.wasm" : "../soccer_core_cpp.39f91010.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
