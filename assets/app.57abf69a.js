e46deeaf9f55a85379a5c50c06420646da3dea70
白河愁 <kryso@21cn.com>
2026-07-28 15:43:38 +0900

    Publish 20260728.9 semantic prematch probes

--- assets/app.57abf69a.js
+++ assets/app.57abf69a.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.f9082dcc.wasm" : "../soccer_core_cpp.39f91010.wasm";
+  const relative = DEBUG ? "../strict-tests.7b69306c.wasm" : "../soccer_core_cpp.c41c8a10.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
