aacde49376f1a72e01d261322f008e2a7c5160ad
白河愁 <kryso@21cn.com>
2026-07-29 12:19:10 +0900

    Publish 20260729.6 bank dispatch catalog

--- assets/app.3ec1fa31.js
+++ assets/app.3ec1fa31.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.78f40a22.wasm" : "../soccer_core_cpp.8b7e6f39.wasm";
+  const relative = DEBUG ? "../strict-tests.01898e1f.wasm" : "../soccer_core_cpp.c072eb0e.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
