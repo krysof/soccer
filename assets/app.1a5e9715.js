afeca6a8c8176b2f637af2f4be7d71ce0920c5b5
白河愁 <kryso@21cn.com>
2026-07-29 13:20:00 +0900

    Publish 20260729.9 continuous playoff oracle

--- assets/app.1a5e9715.js
+++ assets/app.1a5e9715.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.1701ffa7.wasm" : "../soccer_core_cpp.0bca3f43.wasm";
+  const relative = DEBUG ? "../strict-tests.f78d207c.wasm" : "../soccer_core_cpp.32976c3e.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
