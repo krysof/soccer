bc0b28b92bf98c5c3eefe4d70cf3c32f7df9376f
白河愁 <kryso@21cn.com>
2026-07-29 09:49:37 +0900

    Publish Fighting Soccer 20260729.3

--- assets/app.4d608fd4.js
+++ assets/app.4d608fd4.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.175b9ba7.wasm" : "../soccer_core_cpp.7e04e765.wasm";
+  const relative = DEBUG ? "../strict-tests.57216530.wasm" : "../soccer_core_cpp.80d08ea4.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
