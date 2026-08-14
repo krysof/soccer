9d7bd25957a36d475fc058e01771f37a9f179467
白河愁 <kryso@21cn.com>
2026-08-14 14:35:55 +0900

    Release 20260814.2

--- assets/app.2ef7ebc9.js
+++ assets/app.2ef7ebc9.js
@@ -1191,7 +1191,7 @@ function loadOriginalSpriteRendererFromBin(api) {
 }
 async function loadWasm() {
   const filename = DEBUG ? "soccer_core_cpp.wasm" : "soccer_core_cpp_production.wasm";
-  const relative = DEBUG ? "../strict-tests.d2c3ba4f.wasm" : "../soccer_core_cpp.2a399ea7.wasm";
+  const relative = DEBUG ? "../strict-tests.fbb130ed.wasm" : "../soccer_core_cpp.c90446d1.wasm";
   const response = await fetchCoreResponse(filename, assetUrl(relative), rootAssetUrl(filename));
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, {});
